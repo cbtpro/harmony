cb82be1d9a098927683a3a643be53eab19ba8371
chenbitao <cbtpro@163.com>
2023-02-27 08:02:59 +0800

    feat: 添加pinia支持

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useCounterStore } from '../store/counter';
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const store = useCounterStore();
+
+const { count, double, } = storeToRefs(store);
+const { increment } = store;
+
+const test = 123;
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="increment">count is {{ count }}</button>
+    <div>double count is {{ double }}</div>
+    <div>{{ test }}</div>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import './style.css';
 import App from './App.vue';
 import './samples/node-api';
 
-createApp(App)
-  .mount('#app')
+const pinia = createPinia()
+
+const app = createApp(App);
+app.use(pinia);
+
+app.mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*');
   });
